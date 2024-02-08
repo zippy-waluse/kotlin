4ba52cc14427c715ea95e036c50497a4c54ac418
zippy-waluse <zippywaluse@gmail.com>
2024-02-08 21:39:21 +0300

    added new file

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main(){
+  val fullname = "vivian mtule"
+  println(fullname)
+  val age = 19
+  println(age)
+  val phoneNumber = "+25476786345"
+  println(phoneNumber)
+  val weight = 72.9F
+  println(weight)
+  val isMalawian = false
+  println(isMalawian)
+
+
+}
+
